7643a4e9506d3d6986ce085222691a605fbb263b
Joonas Metsaniemi <t0mejo00@students.oamk.fi>
2022-02-10 21:56:23 +0200

    tehtyja

---
+++ excercise1/harjoitus1/src/FlexboxElements/NewContainer.js
@@ -0,0 +1,98 @@
+import React from 'react';
+
+export default function NewContainer(props) {
+  return (<div className='Formatting1'>
+
+           
+            
+            
+
+            <ul className='subHeaderBar'>
+                {
+                    props.subHeaders.map(item =>{
+                        return <div><p className='subHeaders' key={item}> {item} </p></div>
+                    })
+                }
+            </ul>
+
+            <div className='topBar'>
+
+                TESTI
+
+            </div>
+
+            <div className='ThisJust'>
+
+                <div className='ThisJustIn'> TESTITEITEITPAOGMNPAONSG </div>
+                <div className='ThisJustIn'> TESTITEITEITPAOGMNPAONSG </div>
+                <div className='ThisJustInCyan'> TESTITEITEITPAOGMNPAONSG </div>
+
+            </div>
+
+        <div className='doggyBar'><h1>DOGGYDOGGYDOGGYDOGGYDOGGYDOGGY KORONA!!!!</h1></div>
+
+        <div className='pictureFraming'>
+
+            <img className='kuva'src='https://stockpetphotography.com/wp-content/uploads/2019/01/stock-pet-photography-dog-breeds3.jpg'></img>
+
+
+            <div className='NewsBar'>
+                <h3>LUETUIMMAT</h3>
+                <div className='NewNews'><div className='numerot'><h2>1</h2></div>  JUin, tottadasdas vai tarua?</div>
+                <div className='NewNews'>TEsddsdsdstiteistiasasgafdshsotk apofjkapso fjk aspogjaspogfdoshndsifoh dfsoih dfoi hdfsoihmdfs giomdf gpoidsfm gpoidmf gpoimdsf gd</div>
+                <div className='NewNews'>UutiaSdasdasdnen</div>
+                <div className='NewNews'>Uutinasdas dddddddddddd dddddddddddddddddddddd ddddddddddddddddddddddddden</div>
+                <div className='NewNews'>TEstiteistiasotk apofjkapso fjk aspogjaspogfdoshndsifoh dfsoih dfoi hdfsoihmdfs giomdf gpoidsfm gpoidmf gpoimdsf gd</div>
+                <div className='NewNews'>TEstiteistiasotk apofjkapso fjk aspogjaspogfdoshndsifoh dfsoih dfoi hdfsoihmdfs giomdf gpoidsfm gpoidmf gpoimdsf gd</div>
+                <div className='NewNews'>TEstiteistiasotk apofjkapso fjk aspogjaspogfdoshndsifoh dfsoih dfoi hdfsoihmdfs giomdf gpoidsfm gpoidmf gpoimdsf gd</div>
+
+
+            </div>
+
+            
+
+        </div>
+
+        <div className='Uutinen'><h1>DOGGYDOGGYDOGGYDOGGYDOGGYDOGGY KORONA!!!!</h1></div>
+        <div className='Uutinen'><h1>DOGGYDOGGYDOGGYDOGGYDOGGYDOGGY KORONA!!!!</h1></div>
+        <div className='Uutinen'><h1>DOGGYDOGGYDOGGYDOGGYDOGGYDOGGY KORONA!!!!</h1></div>
+        <div className='Uutinen'><h1>DOGGYDOGGYDOGGYDOGGYDOGGYDOGGY KORONA!!!!</h1></div>
+        <div className='Uutinen'><h1>DOGGYDOGGYDOGGYDOGGYDOGGYDOGGY KORONA!!!!</h1></div>
+        <div className='Uutinen'><h1>DOGGYDOGGYDOGGYDOGGYDOGGYDOGGY KORONA!!!!</h1></div>
+        <div className='Uutinen'><h1>DOGGYDOGGYDOGGYDOGGYDOGGYDOGGY KORONA!!!!</h1></div>
+        <div className='Uutinen'><h1>DOGGYDOGGYDOGGYDOGGYDOGGYDOGGY KORONA!!!!</h1></div>
+        <div className='Uutinen'><h1>DOGGYDOGGYDOGGYDOGGYDOGGYDOGGY KORONA!!!!</h1></div>
+        
+
+
+        
+
+
+
+        
+
+    
+    
+    
+
+    
+
+    
+
+
+  
+           
+
+
+
+
+    
+
+            
+    </div>
+
+            
+  
+  
+  );
+}
